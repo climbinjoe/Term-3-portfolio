38f9b46dc32134a8dee5864a1eb09fb6425cafb7
Joseph Simper <joseph.simper@tooeleschools.org>
2021-03-18 14:41:12 -0600

    3/18/21 2

---
+++ mad lib.py
@@ -0,0 +1,39 @@
+noun1 = input("enter a noun")
+noun2 = input("enter a noun")
+noun3 = input("enter a noun")
+noun4 = input("enter a noun")
+noun5 = input("enter a noun")
+noun6 = input("enter a noun")
+noun7 = input("enter a noun")
+adjective1 = input("enter an adjective")
+adjective2 = input("enter an adjective")
+adjective3 = input("enter an adjective")
+verb1 = input("enter a verb")
+verb2 = input("enter a verb")
+verb3 = input("enter a verb")
+verb4 = input("enter a verb")
+verb5 = input("enter a verb")
+verb6 = input("enter a verb")
+verb7 = input("enter a verb")
+verb8 = input("enter a verb")
+adverb1 = input("enter an adverb")
+adverb2 = input("enter an adverb")
+
+##var="something else"
+##result=str.format("the {0} string {1} are {2}","testing",var)
+##print(result)
+
+print (str.format(""" The {0} glows {7} on the mountain tonight
+Not a {1} to be seen
+A {2} of isolation
+And it looks like I'm the {3}
+The wind is {8} like this swirling {4} inside
+Couldn't {10} it in, {5} knows I've {9}
+Don't {11} them in, don't {12} them see
+Be the good {6} you {13} have to be
+{14}, don't {15}, don't let them {16}
+Well, now they {17} Let it {18}, let it {19}
+Can't hold it back anymore.""",noun1, noun2, noun3, noun4, noun5, noun6, noun7, 
+adjective1, adjective2, adjective3, verb1, verb2, verb3, verb4, verb5, verb6, verb7, verb8,
+adverb1, adverb2))
+
